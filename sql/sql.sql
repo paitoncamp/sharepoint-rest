ba78b150da8680bda126fdd84f926a47a2ae9956
paitoncamp <paitoncamp@gmail.com>
2021-08-18 19:23:16 +0700

    update site name
    update site name

--- sql/sql.sql
+++ sql/sql.sql
@@ -29,18 +29,18 @@ CREATE TABLE IF NOT EXISTS `config` (
 -- Dumping data for table sp-rest-api.config: ~12 rows (approximately)
 /*!40000 ALTER TABLE `config` DISABLE KEYS */;
 INSERT INTO `config` (`id`, `name`, `value`, `notes`) VALUES
-	(1, 'sp_site_name', 'Paiton Dev', '0'),
-	(2, 'sp_site_path', '/sites/PaitonDev', 'sub sites path only'),
-	(3, 'client_id', '4b6a-a258-a04ff57e', 'sharepoint sites app principals\r\nhttps://paitonoperation.sharepoint.com/sites/PaitonDev/_layouts/15/appprincipals.aspx\r\nto create :\r\nhttps://paitonoperation.sharepoint.com/sites/PaitonDev/_layouts/15/appregnew.aspx\r\n '),
+	(1, 'sp_site_name', 'Example Dev', '0'),
+	(2, 'sp_site_path', '/sites/ExampleDev', 'sub sites path only'),
+	(3, 'client_id', '4b6a-a258-a04ff57e', 'sharepoint sites app principals\r\nhttps://example.sharepoint.com/sites/PaitonDev/_layouts/15/appprincipals.aspx\r\nto create :\r\nhttps://paitonoperation.sharepoint.com/sites/PaitonDev/_layouts/15/appregnew.aspx\r\n '),
 	(4, 'client_secret', '3V33BQ3XhvcyW6EaH4=', '0'),
-	(5, 'app_domain', 'wira.localhost', '0'),
-	(6, 'redirect_uri', 'https://wira.localhost', '0'),
+	(5, 'app_domain', 'example.localhost', '0'),
+	(6, 'redirect_uri', 'https://example.localhost', '0'),
 	(7, 'tenant_id', 'b1ce-7114ab0c3a33', 'tenant ID , azure directory ID, see. https://portal.azure.com/#settings/directory'),
 	(8, 'audience_principal_id', '000000000000', 'request get to -> https://<mysite.sharepoint.com>/_vti_bin/client.svc   with header Authorization: Bearer'),
 	(9, 'authorization_code', '9gHGe1IepOmMMOt61_sv3T6dTArDnDSl5SZJbwoV3fYypiM7cTLA5j8xTyj0rpcQr9pfWCp94V-k0CkEHSATlnFvsN_X9UyvcSieJ7E2Z0VdIHjFLZskMBJdp97yGL_sxkc0L4PYwQbC3Qt4DPwbt6fetoKDqvVDzeqk0vwo9s6tNUZDdrSJ_gP3iH9hwslgIAA', '/doAzureACS'),
 	(10, 'access_token', '4QUNFMDM3NiJ9.Y9Pkaqk6Co376C9v6EcajReNwZFH0TXODKVNVC6lyGrAi1sPgCnAyq1lD18N_niSb4HyGV6_aw4gvkjhc-zPCMWYMNfNjQkadPDP_sNjUVKVvPrh88vV9Pe2uraWIDOvEMMYwBoq8onMfHtcm7UsHKm8QwPUu8xXm3TUoCCyACf0-T_02TSFeStKtaKj9cVY7DjCI2NYg4Cei--d5vzgzGBEKtxvsh2yXaE9fbZbL-1tmd-aFz54eGREJoXt5BiKsqYlns57lUE3S8exF6_EoY9Se-MnYv8p-NrNSlYa0jqn5Rp_fhTUFv4Q_T86o8', ''),
 	(11, 'refresh_token', 'sdbHxv4_xcHQinZF3YUHbfcnNR3dBoyAA', '/doAccessToken'),
-	(13, 'sp_site', 'paitonoperation.sharepoint.com', 'main sharepoint site name');
+	(13, 'sp_site', 'example.sharepoint.com', 'main sharepoint site name');
 /*!40000 ALTER TABLE `config` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
